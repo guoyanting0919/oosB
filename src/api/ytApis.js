f176e5dacbe58da819b17ba453e2454cd97548d6
YT <guoyanting0919@gmail.com>
2020-09-02 16:29:42 +0800

    V 1.0.0

--- src/api/ytApis.js
+++ src/api/ytApis.js
@@ -1,55 +1,14 @@
 import request from "@/utils/request";
 
 const api = {
-  // 獲取所有序號
-  GetRegisterNo(params) {
+  // forgetPw
+  ForgetPw(data) {
     return request({
-      url: "ActivityTbls/Load",
-      method: "get",
-      params,
-    });
-  },
-  // 創建活動序號
-  CreatRegisterNo(data) {
-    return request({
-      url: `ActivityTbls/GetNo`,
+      url: "/Users/ForgetPwd",
       method: "post",
       data,
     });
   },
-  // 序號單補單
-  SubmitForm(data) {
-    return request({
-      url: `ActivityTbls/Update`,
-      method: "post",
-      data,
-    });
-  },
-  // 匯出抽獎名單  0->明碼 1->隱碼 2->總序號下載
-  // ExportExcel(params){
-  //   return request({
-  //     url: "ActivityTbls/GetExcel",
-  //     method: "get",
-  //     params,
-  //   });
-  // }
-
-  // GetRouterTree(params) {
-  //   return request({
-  //     url: "/Check/GetModulesTree",
-  //     method: "get",
-  //     params,
-  //   });
-  // },
-
-  // // forgetPw
-  // ForgetPw(data) {
-  //   return request({
-  //     url: "/Users/ForgetPwd",
-  //     method: "post",
-  //     data,
-  //   });
-  // },
 };
 
 export default api;
